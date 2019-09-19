91e9d3458602ee11e18b4b9d96f373c5403d9b5b
luissubiabre <luissubiabre@hotmail.com>
2019-09-18 22:35:16 -0300

    Test moving to console message

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
-console.log('Here`s a hidden message');
 let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+console.log('Here`s a hidden message');
